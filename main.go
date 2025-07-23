5a156b130a31f047ecd135bea6aeaebd7ad6a40f
William Kopaczewski <w_kopa@yahoo.com>
2025-07-23 10:28:43 -0400

    refactored getting state into new function

--- main.go
+++ main.go
@@ -16,28 +16,12 @@ type state struct {
 }
 
 func main() {
-	gator_config, err := config.Read()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	db, err := sql.Open("postgres", gator_config.DB_url)
-	if err != nil {
-		log.Fatal("error connecting to db: ", err)
-	}
-
-	dbQueries := database.New(db)
-
-	// Create Florida
-	gator_state := state{
-		cfg: gator_config,
-		db: dbQueries,
-	}
-
+	// Get Florida
+	gator_state := get_state()
 	cmds := get_commands()
 	cmd := parse_command(os.Args)
 
-	err = cmds.run(&gator_state, cmd)
+	err := cmds.run(gator_state, cmd)
 
 	if err != nil {
 		log.Fatal("error running command ", os.Args[1], ": ", err)
@@ -66,4 +50,26 @@ func parse_command(args []string) command {
 		Name: os.Args[1],
 		Args: cmd_args,
 	}
+}
+
+func get_state() *state {
+	gator_config, err := config.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := sql.Open("postgres", gator_config.DB_url)
+	if err != nil {
+		log.Fatal("error connecting to db: ", err)
+	}
+
+	dbQueries := database.New(db)
+
+	// Create Florida
+	gator_state := state{
+		cfg: gator_config,
+		db: dbQueries,
+	}
+
+	return &gator_state
 }
